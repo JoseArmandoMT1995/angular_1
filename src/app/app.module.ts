fc95222ac80e05bd4db5d0ef72152e946afe00b8
JoseArmandoMT1995 <armando.moreno.tolentino@gmail.com>
2021-08-03 14:36:49 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoComponent
   ],
   imports: [
     BrowserModule
